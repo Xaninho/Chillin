86fb4a6df8ecd960d0d9bcec5981c4696cda3d79
Xaninho <neves.27.17@gmail.com>
2021-04-28 16:47:24 +0100

    Material Bottom Tab, with Drawer

--- App.js
+++ App.js
@@ -1,82 +1,16 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
 import {createDrawerNavigator} from '@react-navigation/drawer';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import {View, Text} from 'react-native';
+import MainTabScreen from './screens/MainTabsScreen';
 
-import MusicScreen from './screens/MusicScreen';
-
-const HomeStack = createStackNavigator();
-const MusicStack = createStackNavigator();
 const Drawer = createDrawerNavigator();
-
-const HomeStackScreen = ({navigation}) => {
-  return (
-    <HomeStack.Navigator>
-      <HomeStack.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          title: 'Home',
-          headerLeft: () => (
-            <Ionicons
-              name="menu"
-              color={'#35355'}
-              size={23}
-              onPress={() => {
-                navigation.openDrawer();
-              }}
-            />
-          ),
-        }}
-      />
-    </HomeStack.Navigator>
-  );
-};
-const MusicStackScreen = ({navigation}) => {
-  return (
-    <MusicStack.Navigator>
-      <MusicStack.Screen
-        name="Music"
-        component={MusicScreen}
-        options={{
-          title: 'Music',
-          headerLeft: () => (
-            <Ionicons
-              name="menu"
-              color={'#35355'}
-              size={23}
-              onPress={() => {
-                navigation.openDrawer();
-              }}
-            />
-          ),
-        }}
-      />
-    </MusicStack.Navigator>
-  );
-};
-
-const HomeScreen = () => {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>HomeScreen</Text>
-    </View>
-  );
-};
 
 export default App = () => {
   return (
     <NavigationContainer>
       <Drawer.Navigator>
-        <Drawer.Screen name="Home" component={HomeStackScreen} />
-        <Drawer.Screen name="Music" component={MusicStackScreen} />
+        <Drawer.Screen name="Home" component={MainTabScreen} />
+        {/*<Drawer.Screen name="Music" component={MusicStackScreen} />*/}
       </Drawer.Navigator>
     </NavigationContainer>
   );
