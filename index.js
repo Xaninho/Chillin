aed06dd86ca93bd3433534ed9ec179cb7a1a7c53
Xaninho <neves.27.17@gmail.com>
2021-05-01 15:42:57 +0100

    Simple Track Play & Pause

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {AppRegistry} from 'react-native';
 import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
+import TrackPlayer from 'react-native-track-player';
+
 import {name as appName} from './app.json';
 import App from './App';
 
@@ -24,3 +26,4 @@ export default Main = () => {
 };
 
 AppRegistry.registerComponent(appName, () => Main);
+TrackPlayer.registerPlaybackService(() => require('./service.js'));
--- screens/MusicScreen.js
+++ screens/MusicScreen.js
@@ -1,17 +1,99 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect} from 'react';
+import {StyleSheet, View, TouchableOpacity, Text} from 'react-native';
 import {useTheme} from 'react-native-paper';
+import TrackPlayer from 'react-native-track-player';
 
-export default FavoritesScreen = () => {
+const tracks = [
+  {
+    id: 1,
+    url: require('../music/blues.wav'),
+    title: 'Blues Beat',
+  },
+  {
+    id: 2,
+    url: require('../music/tracks_country.mp3'),
+    title: 'Blues Beat',
+  },
+];
+
+TrackPlayer.updateOptions({
+  stopWithApp: false,
+  capabilities: [TrackPlayer.CAPABILITY_PLAY, TrackPlayer.CAPABILITY_PAUSE],
+  compactCapabilities: [
+    TrackPlayer.CAPABILITY_PLAY,
+    TrackPlayer.CAPABILITY_PAUSE,
+  ],
+});
+
+export default MusicScreen = () => {
+  const setUpTrackPlayer = async () => {
+    try {
+      await TrackPlayer.setupPlayer();
+      await TrackPlayer.add(tracks);
+      console.log('Tracks added');
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    setUpTrackPlayer();
+
+    return () => TrackPlayer.destroy();
+  }, []);
+
   const colors = useTheme();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text style={{color: colors.whiteSubtle}}>Music! Yay!</Text>
+    <View style={styles.container}>
+      <View style={styles.row}>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => TrackPlayer.pause()}>
+          <Text style={styles.text}>Pause</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn} onPress={() => TrackPlayer.play()}>
+          <Text style={styles.text}>Play</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.row}>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => TrackPlayer.skipToPrevious()}>
+          <Text style={styles.text}>Prev</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => TrackPlayer.skipToNext()}>
+          <Text style={styles.text}>Next</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+  },
+  btn: {
+    backgroundColor: '#ff0044',
+    padding: 15,
+    borderRadius: 10,
+    margin: 10,
+    width: 160,
+  },
+  text: {
+    fontSize: 30,
+    color: 'white',
+    textAlign: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+});
