e00d1f30b85e38e24b1dfdc861b658d7360376c5
Xaninho <neves.27.17@gmail.com>
2021-05-07 12:15:05 +0100

    List Menu with Delete & Clipboard Options

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,19 +1,23 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, FlatList, StyleSheet} from 'react-native';
-import {useTheme, List} from 'react-native-paper';
+import {View, FlatList, StyleSheet, ToastAndroid} from 'react-native';
+import {useTheme} from 'react-native-paper';
 import {useIsFocused} from '@react-navigation/native';
-
-import realm from '../database/allSchemas';
 import {
-  queryAllFavorites,
-  insertFavorite,
-  deleteFavorite,
-} from '../database/allSchemas';
+  MenuProvider,
+  Menu,
+  MenuOptions,
+  MenuOption,
+  MenuTrigger,
+} from 'react-native-popup-menu';
+import Clipboard from '@react-native-community/clipboard';
+
+import {queryAllFavorites, deleteFavorite} from '../database/allSchemas';
 
 export default FavoritesScreen = () => {
   const {colors} = useTheme();
   const isFocused = useIsFocused();
   const [favoritesData, setFavoritesData] = useState(0);
+  const [refresh, setRefresh] = useState(false);
 
   useEffect(() => {
     queryAllFavorites()
@@ -23,23 +27,59 @@ export default FavoritesScreen = () => {
       .catch(error => {
         console.log(error);
       });
-  }, [isFocused]);
+  }, [isFocused, refresh]);
+
+  const copyToClipboard = (musicName, musicArtist) => {
+    Clipboard.setString(musicName + ' - ' + musicArtist);
+    ToastAndroid.show(
+      musicName + ' - ' + musicArtist + ' Copied to Clipboard!',
+      ToastAndroid.SHORT,
+    );
+  };
+
+  const deleteFromFavorite = idToDelete => {
+    deleteFavorite(idToDelete)
+      .then(
+        setRefresh(!refresh),
+        ToastAndroid.show('Music Deleted from Favorites', ToastAndroid.SHORT),
+      )
+      .catch(error => {
+        alert('Delete favorite error ' + error);
+      });
+  };
 
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: colors.backgroundDarker,
-      }}>
-      <FlatList
-        style={styles.flatList}
-        data={favoritesData}
-        renderItem={({item}) => (
-          <Text style={styles.item}>{item.songName}</Text>
-        )}
-        keyExtractor={item => item.id}
-      />
-    </View>
+    <MenuProvider>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: colors.backgroundDarker,
+        }}>
+        <FlatList
+          style={styles.flatList}
+          data={favoritesData}
+          extraData={refresh}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => (
+            <Menu>
+              <MenuTrigger text={item.songName} style={styles.item} />
+              <MenuOptions>
+                <MenuOption
+                  onSelect={() =>
+                    copyToClipboard(item.songName, item.songArtist)
+                  }
+                  text="Copy to Clipboard"
+                />
+                <MenuOption
+                  onSelect={() => deleteFromFavorite(item.id)}
+                  text="Delete"
+                />
+              </MenuOptions>
+            </Menu>
+          )}
+        />
+      </View>
+    </MenuProvider>
   );
 };
 
